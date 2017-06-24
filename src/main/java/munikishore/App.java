04ef5f339b58fc1fda5482a46aac12d386821e45
munikishore87 <munikishore87@gmail.com>
2017-06-24 14:49:45 +0530

    Add files via upload

---
+++ src/main/java/munikishore/App.java
@@ -0,0 +1,13 @@
+package munikishore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
